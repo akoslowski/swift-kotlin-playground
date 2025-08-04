1f40d4d2a513474843d59a25eba90ad0590448e7
akoslowski <2005824+akoslowski@users.noreply.github.com>
2025-08-04 14:50:42 +0200

    Update test

--- Swift/Tests/kotlin-consumer-tests/Tests.swift
+++ Swift/Tests/kotlin-consumer-tests/Tests.swift
@@ -1,9 +1,10 @@
 import Testing
+
 @testable import Kotlib
 
-@Test func testExample() {
-        #expect(KotlibKt.greet() == "Hello from Kotlin Multiplatform!")
-        #expect(KotlibKt.calculate(a: 12, b: 12) == 24)
-        #expect(Person(name: "Alice", age: 30).name == "Alice")
-        #expect(Person(name: "Alice", age: 30).age == 30)
+@Test func testExamples() {
+    #expect(KotlibKt.greet() == "Hello from Kotlin Multiplatform!")
+    #expect(KotlibKt.calculate(a: 12, b: 12) == 24)
+    #expect(Person(name: "Alice", age: 30).getDescription() == "Alice is 30 years old")
+    #expect(Foo(name: "Bob").name == "Bob")
 }
