03649ac68d223b28a4e17b665546b3d55410f312
akoslowski <2005824+akoslowski@users.noreply.github.com>
2025-08-07 09:51:28 +0200

    Minify for better overview

--- Swift/Package.swift
+++ Swift/Package.swift
@@ -1,33 +1,15 @@
 // swift-tools-version: 6.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "swift-kotlin-consumer",
-    platforms: [
-        .iOS(.v18),
-        .macOS(.v15)
-    ],
+    platforms: [.macOS(.v15)],
     dependencies: [
-        .package(
-            name: "Kotlib",
-            path: "../build"
-        )
+        .package(name: "Kotlib", path: "../build")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "kotlin-consumer",
-            dependencies: ["Kotlib"]
-        ),
-
-        .testTarget(
-            name: "kotlin-consumer-tests", 
-            dependencies: [
-                "Kotlib",
-            ]
-        )
+        .executableTarget(name: "kotlin-consumer", dependencies: ["Kotlib"]),
+        .testTarget(name: "kotlin-consumer-tests",dependencies: ["Kotlib"])
     ]
 )
